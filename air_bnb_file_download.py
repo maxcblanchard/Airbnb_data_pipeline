2d22ae1e3e3843aaa21461fa4729696c179f4663
maxblanchard <#Bluehellskc96>
2020-12-07 20:29:58 -0700

    Files stored locally and uploaded to Postgresql

---
+++ air_bnb_file_download.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from bs4 import BeautifulSoup
+import requests
+
+
+def main():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  #if you don't want the GUI to pop up
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('http://insideairbnb.com/get-the-data.html')
+    time.sleep(2)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    denver_lines = soup.find('table', {"class": "table table-hover table-striped denver"})
+    denver_list = []
+    for a in denver_lines.find_all('a', href=True):
+        denver_list.append(a['href'])
+    denver_csv_listings_list = []
+    denver_csv_reviews_list = []
+    denver_csv_gz_listings_list = []
+    denver_csv_gz_calendar_list = []
+    denver_csv_gz_reviews_list = []
+    denver_neighborhood_csv_list = []
+    denver_neighborhood_geojson_list = []
+    for i in denver_list:
+        if 'csv.gz' in i:
+            if 'listings' in i:
+                denver_csv_gz_listings_list.append(i)
+                continue
+            if 'calendar' in i:
+                denver_csv_gz_calendar_list.append(i)
+                continue
+            if 'reviews' in i:
+                denver_csv_gz_reviews_list.append(i)
+                continue
+        if 'neighbourhood' in i:
+            if 'neighbourhoods.csv' in i:
+                denver_neighborhood_csv_list.clear()
+                denver_neighborhood_csv_list.append(i)
+                continue
+            if 'neighbourhoods.geojson' in i:
+                denver_neighborhood_geojson_list.clear()
+                denver_neighborhood_geojson_list.append(i)
+                continue
+        else:
+            if 'listings' in i:
+                denver_csv_listings_list.append(i)
+                continue
+            if 'reviews' in i:
+                denver_csv_reviews_list.append(i)
+                continue
+
+    def save_airbnb_files(file_list, save_length, date_value_1, date_value_2):
+        for z in range(len(file_list)):
+            url = file_list[z]
+            r = requests.get(url)
+            if 'listings' in url:
+                with open('/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline/AirBNB_Downloads/Listings/'
+                          + url[-date_value_1:-date_value_2] + '_' + url[-save_length:], 'wb') as f:
+                    f.write(r.content)
+            if 'reviews' in url:
+                with open('/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline/AirBNB_Downloads/Reviews/'
+                          + url[-date_value_1:-date_value_2] + '_' + url[-save_length:], 'wb') as f:
+                    f.write(r.content)
+            if 'neighbourhood' in url:
+                with open('/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline/AirBNB_Downloads/Neighbourhoods/'
+                          + url[-date_value_1:-date_value_2] + '_' + url[-save_length:], 'wb') as f:
+                    f.write(r.content)
+            print(r.status_code)
+            print(r.headers['content-type'])
+            print(r.encoding)
+
+    #save_airbnb_files(denver_csv_gz_calendar_list, 15, 31, 21)
+    #save_airbnb_files(denver_csv_gz_listings_list, 15, 31, 21)
+    #save_airbnb_files(denver_csv_gz_reviews_list, 14, 30, 20)
+    save_airbnb_files(denver_csv_listings_list, 12, 38, 28)
+    save_airbnb_files(denver_csv_reviews_list, 11, 37, 27)
+    save_airbnb_files(denver_neighborhood_csv_list, 18, 44, 34)
+    #save_airbnb_files(denver_neighborhood_geojson_list, 22, 48, 38)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ postgres_connection.py
@@ -0,0 +1,135 @@
+import psycopg2
+import os
+import csv
+
+conn = psycopg2.connect("host=localhost dbname=AirBNB user=postgres password=Aspen909")
+
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TEMP TABLE IF NOT EXISTS templistings(
+    listing_id integer,
+    listing_name text,
+    host_id	bigint,
+    host_name text,
+    neighbourhood_group text,
+    neighbourhood text,
+    latitude decimal,
+    longitude decimal,
+    room_type text,
+    price decimal,
+    minimum_nights smallint,
+    number_of_reviews smallint,
+    last_review date,
+    reviews_per_month decimal,
+    calculated_host_listings_count smallint,
+    availability_365 smallint
+)
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS denver.listings(
+    listing_id integer PRIMARY KEY ,
+    listing_name text,
+    host_id	bigint,
+    host_name text,
+    neighbourhood_group text,
+    neighbourhood text,
+    latitude decimal,
+    longitude decimal,
+    room_type text,
+    price decimal,
+    minimum_nights smallint,
+    number_of_reviews smallint,
+    last_review date,
+    reviews_per_month decimal,
+    calculated_host_listings_count smallint,
+    availability_365 smallint
+)
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS denver.reviews (
+    listing_id integer,
+    review_date date
+    )
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS denver.neighbourhoods(
+    neighbourhood_group text, 
+    neighbourhood text
+    )
+""")
+
+listings_csv_files = [i for i in os.listdir("/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline/AirBNB_Downloads"
+                                            "/Listings")]
+reviews_csv_files = [i for i in os.listdir("/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline/AirBNB_Downloads"
+                                           "/Reviews")]
+neighborhoods_csv_files = [i for i in os.listdir("/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline"
+                                                 "/AirBNB_Downloads/Neighbourhoods")]
+
+for i in listings_csv_files:
+    if '2016-05-16' in i:
+        break
+    with open("/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline/AirBNB_Downloads/Listings/" + i, 'r') as f:
+        next(f)
+        print(i)
+        cur.copy_expert("""COPY templistings FROM STDIN WITH (FORMAT CSV)""", f)
+        cur.execute("""
+                INSERT INTO denver.listings(
+                    listing_id,
+                    listing_name,
+                    host_id,
+                    host_name,
+                    neighbourhood_group,
+                    neighbourhood,
+                    latitude,
+                    longitude,
+                    room_type,
+                    price,
+                    minimum_nights,
+                    number_of_reviews,
+                    last_review,
+                    reviews_per_month,
+                    calculated_host_listings_count,
+                    availability_365)
+                SELECT distinct 
+                    listing_id,
+                    listing_name,
+                    host_id,
+                    host_name,
+                    neighbourhood_group,
+                    neighbourhood,
+                    latitude,
+                    longitude,
+                    room_type,
+                    price,
+                    minimum_nights,
+                    number_of_reviews,
+                    last_review,
+                    reviews_per_month,
+                    calculated_host_listings_count,
+                    availability_365
+                FROM templistings t
+                WHERE t.listing_id not in (
+                    select listing_id
+                    from denver.listings
+                )
+            """)
+
+for i in reviews_csv_files:
+    with open("/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline/AirBNB_Downloads/Reviews/" + i, 'r') as f:
+        next(f)
+        print(i)
+        cur.copy_expert("""COPY denver.reviews FROM STDIN WITH (FORMAT CSV)""", f)
+
+for i in neighborhoods_csv_files:
+    with open("/Users/maxblanchard/PycharmProjects/Airbnb_data_pipeline/AirBNB_Downloads/Neighbourhoods/"
+              + i, 'r') as f:
+        next(f)
+        cur.copy_expert("""COPY denver.neighbourhoods FROM STDIN WITH (FORMAT CSV)""", f)
+
+
+conn.commit()
+conn.close()
